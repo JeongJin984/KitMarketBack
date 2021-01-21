b574973f474d3109f35cc890438f3465d9dbcbec
unknown <kumdo0159@knu.ac.kr>
2021-01-21 16:33:29 +0900

    m;

---
+++ src/main/java/com/siy/KitMarket/KitMarketApplication.java
@@ -0,0 +1,13 @@
+package com.siy.KitMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siy/KitMarket/KitMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siy.KitMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
